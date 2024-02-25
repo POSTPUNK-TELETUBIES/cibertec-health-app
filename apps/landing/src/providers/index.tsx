f4bc4490ba5de802b10c3199866f4f0ce474d007
Shadow <pieroramirez810@personal.com>
2024-02-25 11:58:15 -0500

    ci: addeds tehhme

--- apps/landing/src/providers/index.tsx
+++ apps/landing/src/providers/index.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PropsWithChildren, useState } from 'react'
 import { SupabaseProvider } from './SupabaseProvider'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { healthTheme } from '@/theme'
+import { healthTheme } from 'ui'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 
 export function Providers({ children }: PropsWithChildren) {
