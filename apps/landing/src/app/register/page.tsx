d4310837ceb08cc8c430ba8392b724c6b3b8ab28
Shadow <pieroramirez810@personal.com>
2024-02-24 23:12:30 -0500

    feat: theme

---
+++ apps/landing/src/app/register/page.tsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Typography>
+        Hai<strong>Doc</strong>
+      </Typography>
+      <Box component={'form'}>
+
+      </Box>
+    </main>
+  );
+}
