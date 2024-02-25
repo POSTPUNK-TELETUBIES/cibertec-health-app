de27c49befc013a1a0cc79a48484a074bba6c2c6
Shadow <pieroramirez810@personal.com>
2024-02-24 23:55:10 -0500

    fix: added theme provider

--- apps/landing/src/providers/index.tsx
+++ apps/landing/src/providers/index.tsx
@@ -3,15 +3,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PropsWithChildren, useState } from 'react'
 import { SupabaseProvider } from './SupabaseProvider'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { healthTheme } from '@/theme'
 
 export function Providers({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
-      <SupabaseProvider>
-        {children}
-      </SupabaseProvider>
+      <ThemeProvider theme={healthTheme}>
+        <CssBaseline />
+        <SupabaseProvider>
+          {children}
+        </SupabaseProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   )
 }
