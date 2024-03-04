0ea11b692efb75262ebc9920404a5eb7dc50a9dc
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-03-04 15:14:34 -0500

    chore: update header

--- apps/landing/src/components/Footer/index.tsx
+++ apps/landing/src/components/Footer/index.tsx
@@ -25,85 +25,83 @@ export const Footer = () => {
   return (
     <Box
       sx={{
-        padding: "2rem",
-        bgcolor: "background.default",
-        color: "text.primary",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        padding: '2rem',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
       }}
     >
-      <Box sx={{ maxWidth: "1500px", width: "100%", textAlign: "center", mb: 4 }}>
+      <Box sx={{ maxWidth: '1500px', width: '100%', textAlign: 'center', mb: 4 }}>
         <Card
           sx={{
-            width: "1500px",
-            height: "417px",
-            borderRadius: "16px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
+            width: '1500px',
+            height: '417px',
+            borderRadius: '16px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
             mb: 14,
           }}
         >
-          <Typography align="center" variant="h2">
-            Empezar{" "}
-            <Typography
-              color="sheldon"
-              fontSize="inherit"
-              fontWeight="inherit"
-              component="span"
-            >
+          <Typography align='center' variant='h2'>
+            Empezar{' '}
+            <Typography color='sheldon' fontSize='inherit' fontWeight='inherit' component='span'>
               con HaiDoc
             </Typography>
           </Typography>
           <Typography
-            variant="body1"
+            variant='body1'
             sx={{
-              justifyContent: "center",
+              justifyContent: 'center',
               my: 2,
-              maxWidth: "70ch", 
+              maxWidth: '70ch',
               mx: 'auto',
-              textAlign: "center", 
+              textAlign: 'center',
             }}
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sodales
-            morbi tristique libero urna sem vitae. Viverra facilisis rhoncus et,
-            nibh nullam vitae laoreet.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sodales morbi tristique libero
+            urna sem vitae. Viverra facilisis rhoncus et, nibh nullam vitae laoreet.
           </Typography>
-          <Button variant="contained" sx={{ mt: 4, borderRadius: "40px" }}  href={`${process.env.PROJECT_URL}/#/`}>
+          <Button
+            variant='contained'
+            sx={{ mt: 4, borderRadius: '40px' }}
+            href={`${process.env.PROJECT_URL}/#/`}
+          >
             Empecemos
           </Button>
         </Card>
-      <Stack
-        direction={{ xs: "column", sm: "row" }}
-        justifyContent="space-between"
-        divider={<Box sx={{ width: "1px", bgcolor: "grey.300", mx: 4 }} />}
-        spacing={2}
-        sx={{
-          width: "100%",
-        }}
-      >
-        {Object.entries(footerLinks).map(([section, links]) => (
-          <Box key={section}>
-            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', fontSize: '20px' }}>
-              {section}
-            </Typography>
-            {links.map((text) => (
-              <Link
-                href="#"
-                key={text}
-                color="inherit"
-                underline="hover"
-                sx={{ display: "block", mb: 0.5, mb: 0.5, fontWeight: 'normal', fontSize: '20px' }}
-              >
-                {text}
-              </Link>
-            ))}
-          </Box>
-        ))}
-      </Stack>
+        <Stack
+          direction={{ xs: 'column', sm: 'row' }}
+          justifyContent='space-between'
+          divider={<Box sx={{ width: '1px', bgcolor: 'grey.300', mx: 4 }} />}
+          spacing={2}
+          sx={{
+            width: '100%',
+          }}
+        >
+          {Object.entries(footerLinks).map(([section, links]) => (
+            <Box key={section}>
+              <Typography variant='h6' gutterBottom sx={{ fontWeight: 'bold', fontSize: '20px' }}>
+                {section}
+              </Typography>
+              {links.map((text) => (
+                <Link
+                  href='#'
+                  key={text}
+                  color='inherit'
+                  underline='hover'
+                  sx={{ display: 'block', mb: 0.5, fontWeight: 'normal', fontSize: '20px' }}
+                >
+                  {text}
+                </Link>
+              ))}
+            </Box>
+          ))}
+        </Stack>
       </Box>
     </Box>
-  );
+  )
 };
---
+++ apps/landing/src/components/Header/index.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import {
+  Box,
+  Divider,
+  ListItemIcon,
+  MenuItem,
+  Paper,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from '@mui/material'
+import { MainLogo } from 'ui'
+import EventIcon from '@mui/icons-material/Event'
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital'
+import MoreTimeIcon from '@mui/icons-material/MoreTime'
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork'
+
+const theme = createTheme()
+const base = process.env.PROJECT_URL!
+
+const Header = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper elevation={0} style={{ zIndex: 2 }} sx={{ background: '#f4feff' }}>
+        <Box
+          display='flex'
+          gap={1}
+          py={2}
+          px={4}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+        >
+          <a href={base} style={{ textDecoration: 'none' }}>
+            <Typography
+              textAlign={'center'}
+              component={'h2'}
+              variant='h2'
+              fontWeight={'lighter'}
+              color={'secondary'}
+              fontSize={'1.7rem'}
+            >
+              Hai<span style={{ fontWeight: 'bold' }}>Doc</span>{' '}
+              <MainLogo style={{ width: '24px', height: '24px', marginBottom: '5px' }} />
+            </Typography>
+          </a>
+
+          <Box display='flex' gap={2}>
+            <a
+              href={`${base}/#/reservarNuevaCita`}
+              style={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              <MenuItem>
+                <ListItemIcon>
+                  <MoreTimeIcon />
+                </ListItemIcon>
+                Reservar nueva cita
+              </MenuItem>
+            </a>
+
+            <Divider />
+            <a
+              href={`${base}/#/misCitasMedicas`}
+              style={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              <MenuItem>
+                <ListItemIcon>
+                  <EventIcon />
+                </ListItemIcon>
+                Mis citas médicas
+              </MenuItem>
+            </a>
+            <a href={`${base}#/verDoctores`} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <MenuItem>
+                <ListItemIcon>
+                  <LocalHospitalIcon />
+                </ListItemIcon>
+                Doctores
+              </MenuItem>
+            </a>
+            <a href={`${base}#/verSucursales`} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <MenuItem>
+                <ListItemIcon>
+                  <MapsHomeWorkIcon />
+                </ListItemIcon>
+                Sucursales
+              </MenuItem>
+            </a>
+          </Box>
+        </Box>
+      </Paper>
+    </ThemeProvider>
+  )
+}
+
+export default Header
